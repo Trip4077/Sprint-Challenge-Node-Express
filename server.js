3bcdeb820f1e999f1e974eb91edaf9fc5117c0af
Trip1701 <bj2336@email.vccs.edu>
2019-03-09 12:27:07 -0500

    projectRoutes linked to Server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const routes = require('./api/routes');
+
 const helmet = require('helmet');
 const logger = require('morgan');
 
 const server = express();
 
 server.use(express.json(), helmet(), logger('dev'));
+server.use('/api', routes);
 
 module.exports = server;
