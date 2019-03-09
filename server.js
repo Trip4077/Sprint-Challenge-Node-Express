716c9e6a4327712feb0153c73640b2683bb76f28
Trip1701 <bj2336@email.vccs.edu>
2019-03-09 14:31:43 -0500

    React App implemented and displaying data

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routes = require('./api/routes');
 
 const helmet = require('helmet');
+const cors = require('cors');
 const logger = require('morgan');
 
 const server = express();
 
-server.use(express.json(), helmet(), logger('dev'));
+server.use(express.json(), helmet(), cors(), logger('dev'));
 server.use('/api', routes);
 
 module.exports = server;
