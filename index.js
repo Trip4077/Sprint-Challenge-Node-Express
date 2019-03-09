90c566efbd64ceacd964ae1b5b7e0faaae60aa89
Trip1701 <bj2336@email.vccs.edu>
2019-03-09 12:19:15 -0500

    Server Setup

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+require('dotenv').config();
+
+const server = require('./server');
+const PORT = process.env.PORT || '4000';
+
+server.listen(PORT, () => {
+    console.log(`\n*** Server listening on port:${PORT} ***\n`)
+});
